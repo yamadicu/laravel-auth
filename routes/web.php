42aeaf39d6aab17e996ab01429062a17cb6f436c
yamadicu <dicutia@gmail.com>
2023-06-22 18:03:47 +0200

    primo push

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [ProjectController::class, 'index'])->name('dashboard');
+    
 });
 
 require __DIR__.'/auth.php';
