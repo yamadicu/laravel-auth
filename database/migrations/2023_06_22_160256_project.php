cf9fd340bacc1576bcd1773f2252b66e27f4654c
yamadicu <dicutia@gmail.com>
2023-06-23 15:16:21 +0200

    inserimento den db

--- database/migrations/2023_06_22_160256_project.php
+++ database/migrations/2023_06_22_160256_project.php
@@ -13,7 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('project', function( Blueprint $table){
+            $table->id();
+            $table->string('title','150')->unique();
+            $table->text('content')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('project');
     }
 };
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Project;
+
+use Illuminate\Support\Str;
+
 class ProjectSeeder extends Seeder
 {
     /**
@@ -12,8 +17,18 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 20; $i++ ){
+
+            for($i = 0; $i < 20; $i++ ){
+                $new_project = new Project();
+                $new_project->title = $faker->sentence(3);
+                $new_project->content = $faker->text(500);
+                $new_project->slug = Str::slug($new_project-title, '-');
+                $new_project->save();
+            }
+
+        }
     }
 }
